62bf0ce952893cddfb800da71adf8a1a3ed14fb1
joi666 <shaparenkoartem@icloud.com>
2025-03-19 18:20:55 +0200

    Created project

---
+++ src/main/java/one/nure/bookhive/BookHiveApplication.java
@@ -0,0 +1,13 @@
+package one.nure.bookhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookHiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/nure/bookhive/BookHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package one.nure.bookhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
